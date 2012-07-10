34e0b872cc24a6f7fa3ce7d0d345b52e02702b30
Mic <misvy@vmware.com>
2012-07-10 11:23:10 +0800

    improved logging

--- src/main/java/aop/pointcut/plus/LogAspect.java
+++ src/main/java/aop/pointcut/plus/LogAspect.java
@@ -8,31 +8,31 @@ public class LogAspect {
 
 	@Before("execution(public * aop.pointcut.plus.ServiceAbstract.*(..))")
 	public void beforeMethod1() { 
-		System.out.println("before");
+		System.out.println("ServiceAbstract.*");
 	}
 	
 	@Before("execution(public * aop.pointcut.plus.ServiceInterface.*(..))")
 	public void beforeMethod2() { 
-		System.out.println("before");
+		System.out.println("ServiceInterface.*");
 	}
 	
 	@Before("execution(public * aop.pointcut.plus.ServiceImpl.*(..))")
 	public void beforeMethod3() { 
-		System.out.println("before");
+		System.out.println("ServiceImpl.*");
 	}
 	
 	@Before("execution(public * aop.pointcut.plus.ServiceAbstract+.*(..))")
 	public void beforeMethod4() { 
-		System.out.println("before");
+		System.out.println("ServiceAbstract+.*");
 	}
 	
 	@Before("execution(public * aop.pointcut.plus.ServiceInterface+.*(..))")
 	public void beforeMethod5() { 
-		System.out.println("before");
+		System.out.println("ServiceInterface+.*");
 	}
 	
 	@Before("execution(public * aop.pointcut.plus.ServiceImpl+.*(..))")
 	public void beforeMethod6() { 
-		System.out.println("before");
+		System.out.println("ServiceImpl+.*");
 	}
 }
--- src/main/java/aop/pointcut/plus/PointcutTest.java
+++ src/main/java/aop/pointcut/plus/PointcutTest.java
@@ -3,6 +3,7 @@ package aop.pointcut.plus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,9 +12,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class PointcutTest {
 	@Autowired
 	private ServiceInterface serviceInterface;
+	
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Test
 	public void hello() {
+		System.out.println(this.applicationContext.getBeanDefinitionCount());
 		serviceInterface.update("toto");
 	}
 
