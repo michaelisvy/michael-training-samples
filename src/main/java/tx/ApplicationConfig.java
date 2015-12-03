2d0d2bfe443bf4b5c86d4f74cc6122d671917d16
michaelisvy <misvy@gopivotal,com>
2015-12-03 09:09:15 +0800

    improvements in tx part
    - no need for @Configuration annotation because we do not use component
    scanning
    - fixed bug in xml config

--- src/main/java/tx/ApplicationConfig.java
+++ src/main/java/tx/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package tx;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -10,7 +9,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
-@Configuration
+// no need for @Configuration annotation because we are not using Component scanning
 public class ApplicationConfig {
 
     @Bean
